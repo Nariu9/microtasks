b4fcf4b96ec9e133929bd13b7f48b4ed08246c06
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-05-25 17:06:07 +0300

    monday, week 1, microtask 4 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,57 @@
-import React from 'react';
+import React, {MouseEvent, useState} from 'react';
 import './App.css';
-import {NewComponent} from "./NewComponent";
+import {Button} from "./components/Button";
 
-/*import {Header} from "./Header";
-import {Body} from "./Body";
-import {Footer} from "./Footer";*/
 
 function App() {
-    const students = [
-        {id: 1, name: "James", age: 8},
-        {id: 2, name: "Robert", age: 18},
-        {id: 3, name: "John", age: 28},
-        {id: 4, name: "Michael", age: 38},
-        {id: 5, name: "William", age: 48},
-        {id: 6, name: "David", age: 58},
-        {id: 7, name: "Richard", age: 68},
-        {id: 8, name: "Joseph", age: 78},
-        {id: 9, name: "Thomas", age: 88},
-        {id: 10, name: "Charles", age: 98},
-        {id: 11, name: "Christopher", age: 100},
-    ]
+    /* const Button1Foo = (subscriber:string, age:number, address:string) => {
+         console.log(subscriber, age, address)
+     }
+
+     const Button2Foo = (subscriber:string) => {
+         console.log(subscriber)
+     }
+
+     const Button3Foo = () => {
+         console.log("I'm stupid button")
+     }*/
+
+    /*let a = 1*/
+    let[a, setA] = useState(1)
+
+    const onClickHandler =()=> {
+        setA(++a);
+        console.log(a)
+    }
+
+    const toZero =()=> {
+        setA(0);
+        console.log(a)
+    }
+
+
     return (
-        <>
-            <NewComponent students={students}/>
-        </>
+        <div className="App">
+            {/* <Button name={"MyYouTubeChannel-1"} callBack={()=>Button1Foo("I'm Vasya", 21, "Live in Minsk")}/>
+            <Button name={"MyYouTubeChannel-2"} callBack={()=>Button2Foo("I'm Ivan")}/>
+            <Button name={"Yo Yo"} callBack={Button3Foo}/>*/}
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number</button>
+            <button onClick={toZero}>0</button>
+
+        </div>
     );
 }
 
 export default App;
+
+/*const [money, setMoney] = useState([
+    { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+    { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+    { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+])*/
